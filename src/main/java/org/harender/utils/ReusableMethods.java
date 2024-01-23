a5794bf03dff283174f982eed17797b7fdf31577
Harender Soni <hsoni2010@gmail.com>
2024-01-23 11:21:01 +0530

    in Reusable Methods class 1- Switch To Handling 2- Alert Handling Methods 3- Navigation Handling Methods 4- Actions Handling Random Scenarios - In Progress

--- src/main/java/org/harender/utils/ReusableMethods.java
+++ src/main/java/org/harender/utils/ReusableMethods.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.ie.*;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.*;
 
 import java.io.*;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.util.Date;
@@ -15,11 +17,195 @@ import java.util.Date;
 public class ReusableMethods {
 
     private static WebDriver driver;
+    private static WebDriverWait wait;
+    private JavascriptExecutor jsExecutor;
+    private Actions actions;
     private static boolean scrnShotFldrNotAvailable=true;
 
     // Specify the path where you want to create the folder
     private static String path = "C:\\Users\\User\\Documents\\GitHub\\SeleniumWithTestng\\ScreenShotsDirectory\\";
     private static String scrnShotFolderPath;
+
+    public ReusableMethods(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        this.jsExecutor = (JavascriptExecutor) driver;
+        this.actions = new Actions(driver);
+    }
+
+    public void waitForVisibilityOfAllElements(By anyLocator) {
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(anyLocator));
+    }
+    public void waitForElementToBeClickableAndClick(By anyLocator) {
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(anyLocator));
+        element.click();
+    }
+    public void selectByValueFromDropdown(By dropdownLocator, String valueToSelect) {
+        WebElement dropdownElement = driver.findElement(dropdownLocator);
+        Select select = new Select(dropdownElement);
+        select.selectByValue(valueToSelect);
+    }
+
+//------------------- Switch To Handling --------------------------------------------------------
+
+    private static Alert switchToAlert(){
+        try {
+            waitForAlertPresence();
+            return driver.switchTo().alert();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+            return null;
+        }
+    }
+    public static void switchToParentFrame() {
+        driver.switchTo().parentFrame();
+    }
+    public static void switchToFrame(int frameIndex) {
+        driver.switchTo().frame(frameIndex);
+    }
+    public static void switchToFrame(String frameNameOrId) {
+        driver.switchTo().frame(frameNameOrId);
+    }
+    public static void switchToFrame(By frameLocator) {
+        WebElement frameElement = driver.findElement(frameLocator);
+        driver.switchTo().frame(frameElement);
+    }
+    // Switch to the default content (out of any frames)
+    public static void switchToDefaultContent() {
+        driver.switchTo().defaultContent();
+    }
+    public static void switchToWindow(String byWindowHandle) {
+        driver.switchTo().window(byWindowHandle);
+    }
+    public static void switchToWindow(int byWindowIndex) {
+        String[] windowHandles = driver.getWindowHandles().toArray(new String[0]);
+        if(windowHandles.length>=byWindowIndex)
+            driver.switchTo().window(windowHandles[byWindowIndex]);
+        else
+            System.out.println("Total Window Handles Available - "+windowHandles.length+
+                               "Received Window Index to Switch - "+byWindowIndex);
+    }
+    public static void switchToNewWindowAndCloseCurrent() {
+        String currentWindowHandle = driver.getWindowHandle();
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!windowHandle.equals(currentWindowHandle)) {
+                driver.switchTo().window(windowHandle);
+                driver.close();
+                break;
+            }
+        }
+    }
+    // Switch to an iframe using its index, then perform an action, and switch back to default content
+    public static void switchToIframePerformActionSwitchBack(int iframeIndex, By elementLocator) {
+        switchToFrame(iframeIndex);
+        driver.findElement(elementLocator).click();
+        // Switch back to default content
+        switchToDefaultContent();
+    }
+    // Switch to an alert, get its text, and accept it
+    public static String switchToAlertGetTextAndAccept() {
+        switchToAlert();
+        String alertText=getTextFromAlert();
+        acceptAlert();
+        return alertText;
+    }
+    // Switch to an alert, get its text, and dismiss it
+    public String switchToAlertGetTextAndDismiss() {
+        switchToAlert();
+        String alertText=getTextFromAlert();
+        dismissAlert();
+        return alertText;
+    }
+
+
+
+//------------------- Actions Handling Random Scenarios-----------------------------------------------------
+
+
+//--------------- Alert Handling Methods --------------------------------------------------------------------
+
+    private static void waitForAlertPresence(){
+        try {
+            wait.until(ExpectedConditions.alertIsPresent());
+        }catch (NoAlertPresentException e){
+            System.out.println("Alert Presence Time over - No Alert Found \n Exception Message - "+e.getMessage());
+        }
+    }
+    public static void dismissAlert(By closeBtn){
+        try {
+            switchToAlert();
+            driver.findElement(closeBtn).click();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+        }
+    }
+    public static void dismissAlert(){
+        try {
+            switchToAlert().dismiss();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+        }
+    }
+    public static void acceptAlert(){
+        try {
+            switchToAlert().accept();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+        }
+    }
+    public static String getTextFromAlert(){
+        try {
+            return switchToAlert().getText();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+            return "No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage();
+        }
+    }
+    public static String getTextFromAlert(By alertMsg){
+        try{
+            switchToAlert();
+            return driver.findElement(alertMsg).getText();
+        }catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+            return "No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage();
+        }
+    }
+    public static void sendKeysToAlert(String stringToSend){
+        try{
+            switchToAlert().sendKeys(stringToSend);
+        }
+        catch (NoAlertPresentException e){
+            System.out.println("No Alert Present \n Cause : "+e.getCause()+"\n Getting Message - "+e.getMessage());
+        }
+    }
+
+//------------------- Navigation Handling Methods --------------------------------------------------------
+
+    public static void navigateBack() {
+        driver.navigate().back();
+    }
+    public static void navigateForward() {
+        driver.navigate().forward();
+    }
+    public static void refreshUsingNavigate() {
+        driver.navigate().refresh();
+    }
+    public static void navigateToURL(URL url) {
+        driver.navigate().to(url);
+    }
+    public static void navigateToURL(String urlAsString) {
+        driver.navigate().to(urlAsString);
+    }
+
+
+
+
+
+
+
+
+
+
 
     // Initialize WebDriver with default settings
     public static WebDriver initializeDriver() {
